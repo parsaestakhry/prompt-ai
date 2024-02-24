d7919f48d3894fd9d22ccf67d5cc83380764c4c1
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-24 08:44:31 +0330

    commit

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -5,7 +7,13 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image src="/assets/images/logo.svg" alt="logo" width={30} height={30} className="object-contain"/>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
