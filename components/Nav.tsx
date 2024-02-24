ce8f720208bdb40d63ae10c1d7c39f8b336317a8
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-24 08:52:08 +0330

    sign out

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,11 +7,35 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+
+  const isUserLoggedIn = true
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image src="/assets/images/logo.svg" alt="logo" width={30} height={30} className="object-contain"/>
+        <p className="logo_text">Prompt-AI</p>
       </Link>
+      {/* mobile navigation */}
+      <div className="sm:flex hidden">
+          {isUserLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+              <Link href="/create-prompt" className="black_btn">
+                Create Post
+              </Link>
+              <button type="button" onClick={signOut} className="outline_btn">
+                Sign Out
+              </button>
+              <Link href="/profile">
+                <Image src="/assets/images/logo.svg" alt="profile logo" width={37} height={37} className="rounded-full" />
+              </Link>
+            </div>
+          ) : (
+            <>
+            
+            </>
+          )}
+      </div>
     </nav>
   );
 };
