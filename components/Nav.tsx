c38adba685dbe74ce08e513c19e35cdf7780e1cc
Parsa Estakhri <parsaestakhry03@gmail.com>
2024-02-24 09:13:18 +0330

    commit

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import Link from "next/link";
@@ -7,34 +7,84 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
 
-  const isUserLoggedIn = true
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const handleProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    handleProviders();
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image src="/assets/images/logo.svg" alt="logo" width={30} height={30} className="object-contain"/>
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
         <p className="logo_text">Prompt-AI</p>
       </Link>
       {/* mobile navigation */}
       <div className="sm:flex hidden">
-          {isUserLoggedIn ? (
-            <div className="flex gap-3 md:gap-5">
-              <Link href="/create-prompt" className="black_btn">
-                Create Post
-              </Link>
-              <button type="button" onClick={signOut} className="outline_btn">
-                Sign Out
-              </button>
-              <Link href="/profile">
-                <Image src="/assets/images/logo.svg" alt="profile logo" width={37} height={37} className="rounded-full" />
-              </Link>
-            </div>
-          ) : (
-            <>
-            
-            </>
-          )}
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                alt="profile logo"
+                width={37}
+                height={37}
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider: any) => (
+                <button type="button" key={provider.name} className="black" onClick={() => signIn(provider.id)}>
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              alt="profile logo"
+              width={37}
+              height={37}
+              className="rounded-full"
+
+            />
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider: any) => (
+                <button type="button" key={provider.name} className="black" onClick={() => signIn(provider.id)}>
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
       </div>
     </nav>
   );
